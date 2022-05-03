6d638d8632d66975020edef5fe1c4151bb60cc15
weliton <welingtonzsilva@gmail.com>
2022-05-03 10:34:25 -0300

    implementada rota para pet

--- config/routes.php
+++ config/routes.php
@@ -4,6 +4,7 @@
 use Slim\App;
 use Teste\Application\Controllers\FindUserController;
 use Teste\Application\Controllers\HomeController;
+use Teste\Application\Controllers\ListPetController;
 use Teste\Application\Controllers\ListUserController;
 
 return function(App $app){
@@ -11,4 +12,5 @@ return function(App $app){
     $app->get('/',HomeController::class.':index');
     $app->get('/user',ListUserController::class.':index');
     $app->get('/user/{id}',FindUserController::class.':index');
+    $app->get('/pet',ListPetController::class.':index');
 };
