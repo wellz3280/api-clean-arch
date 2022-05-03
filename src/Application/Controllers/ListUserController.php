56e065cf0817bab7648c0ba57322e77d4d2c40a2
weliton <welingtonzsilva@gmail.com>
2022-05-02 23:33:11 -0300

    controller para usuario implementado

---
+++ src/Application/Controllers/ListUserController.php
@@ -0,0 +1,21 @@
+<?php
+    declare(strict_types=1);
+    namespace Teste\Application\Controllers;
+
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Teste\Application\UseCase\InputBoundry;
+use Teste\Application\UseCase\Users\ListUser;
+
+final class ListUserController implements InterfaceController
+{
+    public function index(Request $request, Response $response):Response
+    {
+        $listUser = new ListUser();
+    
+        $load = json_encode($listUser->apply(new InputBoundry())->output(), JSON_PRETTY_PRINT);
+        $response->getBody()->write($load);
+        
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+}
