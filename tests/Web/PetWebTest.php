d38bb9c0bf59077f06ac4525c5f49567074ce9e1
weliton <welingtonzsilva@gmail.com>
2022-05-19 14:39:41 -0300

    implementado testes para pets

---
+++ tests/Web/PetWebTest.php
@@ -0,0 +1,46 @@
+<?php
+    declare(strict_types=1);
+    namespace Test\Web;
+
+    use PHPUnit\Framework\TestCase;
+
+final class PetWebTest extends TestCase
+{
+    public function testDeveRetornarListaDePetsEmFormatoJson()
+    {
+        $url = file_get_contents("http://localhost:8888/api/petshoposasco/pets");
+        $json = json_decode($url);
+
+        self::assertStringNotContainsString("200 ok",$http_response_header[0]);
+        self::assertIsArray($json);
+        self::assertCount(5,$json);
+
+        self::assertSame('cabra do capiroto',$json[1]->name);
+    }
+
+    public function testDeveRetornarBuscaDePetsPoId()
+    {
+        $url = file_get_contents("http://localhost:8888/api/petshoposasco/pets/1");
+        $json = json_decode($url);
+
+        self::assertStringNotContainsString("200 ok",$http_response_header[0]);
+        self::assertIsArray($json);
+        self::assertCount(1,$json);
+
+        self::assertSame('do ruim',$json[0]->name);
+    }
+
+    public function testDeveRetornarEFiltrarPetsPorParametros()
+    {
+         $url = file_get_contents("http://localhost:8888/api/petshoposasco/pets/raca/pintcher");
+        $json = json_decode($url);
+
+        self::assertStringNotContainsString("200 ok",$http_response_header[0]);
+        self::assertIsArray($json);
+        self::assertCount(2,$json);
+
+        self::assertSame('do ruim',$json[0]->name);
+        self::assertSame('cabra do capiroto',$json[1]->name);
+    }
+    
+}
